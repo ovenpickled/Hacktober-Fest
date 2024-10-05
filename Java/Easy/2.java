5ac5a883390928f4cdd81b6f77a709466748b488
san-coded <145939056+san-coded@users.noreply.github.com>
2024-10-05 13:00:23 +0530

    Update 2.java

--- Java/Easy/2.java
+++ Java/Easy/2.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3};
-        System.out.println(numbers(0));
+        int[] numbers = [1, 2, ];
+        System.out.println(numbers[0]);
     }
 }
