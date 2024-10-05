e5fa3394ab8c94e9a888602dd740a9d21fbb1056
awwwyan <109622291+ovenpickled@users.noreply.github.com>
2024-10-05 12:29:36 +0530

    Update 7.py

--- Python/Easy/7.py
+++ Python/Easy/7.py
@@ -1,19 +1,20 @@
-None = "some_value"
+None1 = "some_value"
 if None == "some_value":
     print("This shouldn't work, None is a keyword!")
-True = 1
+True1 = 1
 if True: 
     print("This will cause an error because 'True' is a reserved keyword.")
 
-def for(x):
+x=5
+def for1(x):
     return x * 2
 
-result = for(5)
+result = for1(5)
 print(result)  
 
-if return > 5:
+if result > 5:
     print("You can't use 'return' as a variable name!")
 
-class = "AdvancedPython"
+class1 = "AdvancedPython"
 print("This won't work because 'class' is a reserved keyword in Python.")
 
