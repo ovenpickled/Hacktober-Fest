bce246b45dd46430dfd26a24ab9f61c1a3c2b197
san-coded <145939056+san-coded@users.noreply.github.com>
2024-10-05 12:38:37 +0530

    Update 17.py

--- Python/Easy/17.py
+++ Python/Easy/17.py
@@ -1,11 +1,11 @@
-a=int(input(‘enter a num’))
-b=int(input(‘enter another number’))
+a=int(input("enter a num"))
+b=int(input("enter another number"))
 Sum=a+b
-print(‘Sum=’,sum)
+print("Sum=",Sum)
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 Sum = a + b
-print("Sum: ", Sum)  # Incorrect print syntax for variables
+print("Sum: ",Sum)  # Incorrect print syntax for variables
 
 
