acbd443ef95861a3bebed20112ec58d60750c9ba
san-coded <145939056+san-coded@users.noreply.github.com>
2024-10-05 13:52:46 +0530

    Update 51.py

--- Python/Medium/51.py
+++ Python/Medium/51.py
@@ -1,11 +1,14 @@
 def factorial(n):
  if n == 0:
- return 1
- return n * factorial(n - 1)
- def main():
+    return 1
+ else:
+   return n * factorial(n - 1)
+ 
+def main():
  try:
- result = factorial(-5)
- print(f"Factorial: {result}")
+    result = factorial(5)
+    print(f"Factorial: {result}")
  except RecursionError as e:
- print(f"RecursionError: {e}")
- main()
+    print(f"RecursionError: {e}")
+ 
+main()
